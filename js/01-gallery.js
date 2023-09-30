9603ffe58ce0102b5d55e07e335ea1b25630d825
Nazakin <134816178+Nazakin@users.noreply.github.com>
2023-09-30 21:43:40 +0200

    horny

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,21 @@
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryElement = document.querySelector('.gallery');
+const gallery = galleryItems.map((image) => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${image.original}">
+      <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+    </a>
+  </li>
+`);
 
+galleryElement.innerHTML = gallery.join('');
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+  captionsData: "alt",
+  // overlayOpacity: 0.7,
+});
 console.log(galleryItems);
