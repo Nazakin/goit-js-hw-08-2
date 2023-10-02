3f8e925dfed99ca73f1cd4ec5d3fa5dc12990928
Nazakin <134816178+Nazakin@users.noreply.github.com>
2023-10-02 21:24:04 +0200

    aAa

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+const localStorageKey = "feedback-form-state";
+console.log(localStorageKey);
+
+function saveFormValue() {
+    const formValue = {
+        email: emailInput.value,
+        message: messageInput.value,
+    }
+    localStorage.setItem(localStorageKey, JSON.stringify(formValue));
+};
+emailInput.addEventListener('input', throttle(saveFormValue, 500));
+messageInput.addEventListener('input', throttle(saveFormValue, 500));
+
+window.addEventListener('load', () => {
+    const storedData = localStorage.getItem(localStorageKey);
+    if (storedData) {
+        const formData = JSON.parse(storedData);
+        emailInput.value = formData.email;
+        messageInput.value = formData.message;
+    }
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = {
+        email: emailInput.value,
+        message: messageInput.value,
+      };
+      console.log('Form data:', formData);
+  localStorage.removeItem(localStorageKey);
+
+  emailInput.value = '';
+  messageInput.value = '';
+}
+
+)
