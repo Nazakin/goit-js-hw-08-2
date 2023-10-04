3f8d8306adfa603f60861839c3d1c98e5e7c35a7
Nazakin <134816178+Nazakin@users.noreply.github.com>
2023-10-04 15:55:14 +0200

    final

--- dist/02-video.a74b541c.js
+++ dist/02-video.a74b541c.js
@@ -508,33 +508,13 @@ var _player = require("@vimeo/player");
 var _playerDefault = parcelHelpers.interopDefault(_player);
 var _lodashThrottle = require("lodash.throttle");
 var _lodashThrottleDefault = parcelHelpers.interopDefault(_lodashThrottle);
-const localStorageKey = "videoplayer-current-time";
 const iframe = document.querySelector("iframe");
-const player = new Vimeo.Player(iframe);
-player.on("play", function() {
-    console.log("played the video!");
-});
-player.on("play", onPlay);
-player.getVideoTitle().then(function(title) {
-    console.log("title:", title);
-});
-player.on("timeupdate", (data)=>{
-    const currentTime = data.seconds;
-    localStorage.setItem("videoplayer-current-time", currentTime);
-});
-const updateCurrentTime = (0, _lodashThrottleDefault.default)((currentTime)=>{
-    localStorage.setItem("videoplayer-current-time", currentTime);
-}, 1000);
-window.addEventListener("DOMContentLoaded", ()=>{
-    const savedTime = localStorage.getItem("videoplayer-current-time");
-    if (savedTime !== null) player.setCurrentTime(savedTime).then(function() {
-        player.play().then(function() {}).catch(function(error) {
-            console.error("\u041F\u043E\u043C\u0438\u043B\u043A\u0430 \u043F\u0440\u0438 \u0441\u043F\u0440\u043E\u0431\u0456 \u0432\u0456\u0434\u0442\u0432\u043E\u0440\u0438\u0442\u0438 \u0432\u0456\u0434\u0435\u043E:", error.message);
-        });
-    }).catch(function(error) {
-        console.error("\u041F\u043E\u043C\u0438\u043B\u043A\u0430 \u043F\u0440\u0438 \u0432\u0441\u0442\u0430\u043D\u043E\u0432\u043B\u0435\u043D\u043D\u0456 \u0447\u0430\u0441\u0443 \u0432\u0456\u0434\u0442\u0432\u043E\u0440\u0435\u043D\u043D\u044F:", error.message);
-    });
-});
+const player = new (0, _playerDefault.default)(iframe);
+const localKeyStorage = "videoplayer-current-time";
+player.on("timeupdate", (0, _lodashThrottleDefault.default)(function({ seconds  }) {
+    localStorage.setItem(localKeyStorage, seconds);
+}, 1000));
+player.setCurrentTime(localStorage.getItem(localKeyStorage) || 0);
 
 },{"@vimeo/player":"kmmUG","lodash.throttle":"bGJVT","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"kmmUG":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,35 +1,14 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-const localStorageKey = "videoplayer-current-time";
 
 const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
-
-    player.on('play', function() {
-        console.log('played the video!');
-    });
-    player.on('play', onPlay);
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
-    player.on('timeupdate', (data) => {
-      const currentTime = data.seconds;
-      localStorage.setItem('videoplayer-current-time', currentTime);
-    });
-    const updateCurrentTime = throttle((currentTime) => {
-      localStorage.setItem('videoplayer-current-time', currentTime);
-    }, 1000);
-    window.addEventListener('DOMContentLoaded', () => {
-      const savedTime = localStorage.getItem('videoplayer-current-time');
-      if (savedTime !== null) {
-        player.setCurrentTime(savedTime).then(function () {
-          player.play().then(function () {
-          }).catch(function (error) {
-            console.error('Помилка при спробі відтворити відео:', error.message);
-          });
-        }).catch(function (error) {
-          console.error('Помилка при встановленні часу відтворення:', error.message);
-        });
-      }
-    });
+const player = new Player(iframe);
+const localKeyStorage = 'videoplayer-current-time';
+player.on(
+  'timeupdate',
+  throttle(function ({ seconds }) {
+    localStorage.setItem(localKeyStorage, seconds);
+  }, 1000)
+);
+player.setCurrentTime(localStorage.getItem(localKeyStorage) || 0);
     
